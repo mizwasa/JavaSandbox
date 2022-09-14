9b04e97294c36cfa9d4deabae519697343d39e6b
mizwasa <bonfirelight0413@gmail.com>
2022-09-15 04:17:43 +0900

    オブジェクトの等価性検証

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        
+        Point point1 = new Point(3,2);
+        Point point2 = new Point(3,2);
+
+        System.out.println(point1);
+        System.out.println(point2);
+
+        System.out.println(point1.hashCode());
+        System.out.println(point2.hashCode());
+
+        System.out.println(point1.equals(point2));
+        
     }
 }
